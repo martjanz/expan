de5da5ff018ad04716a8d1ed8c6c20fda1781f4d
Martín <martin.anzorena@gmail.com>
2016-05-11 15:14:38 -0300

    Added loading spinner (spin.js)

--- main.js
+++ main.js
@@ -13,6 +13,30 @@ var defaults = {
 	title: ""
 };
 
+// spinner settings
+var spinnerOpts = {
+	lines: 13 // The number of lines to draw
+	, length: 28 // The length of each line
+	, width: 14 // The line thickness
+	, radius: 42 // The radius of the inner circle
+	, scale: 0.5 // Scales overall size of the spinner
+	, corners: 1 // Corner roundness (0..1)
+	, color: '#eee' // #rgb or #rrggbb or array of colors
+	, opacity: 0.15 // Opacity of the lines
+	, rotate: 0 // The rotation offset
+	, direction: 1 // 1: clockwise, -1: counterclockwise
+	, speed: 1.4 // Rounds per second
+	, trail: 60 // Afterglow percentage
+	, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+	, zIndex: 2e9 // The z-index (defaults to 2000000000)
+	, className: 'spinner' // The CSS class to assign to the spinner
+	, top: '50%' // Top position relative to parent
+	, left: '50%' // Left position relative to parent
+	, shadow: true // Whether to render a shadow
+	, hwaccel: true // Whether to use hardware acceleration
+	, position: 'absolute' // Element positioning
+}
+
 defaults.width = $("#chart").innerWidth() - (defaults.margin.left + defaults.margin.right);
 defaults.height = window.innerHeight * .90 - (defaults.margin.top + defaults.margin.bottom);
 
@@ -388,11 +412,20 @@ function main(o, data) {
 	}
 }
 
+
+// spinner target selector
+var spinTarget =  document.getElementById('chart');
+
 function load(datafile) {
 	// Warning: hardcode indicating amount of fields that are values, not keys.
 	var valueProps = 1;
 
+	// spinner loader
+	var spinner = new Spinner(spinnerOpts).spin(spinTarget);
+
 	d3.csv(datafile, function(d) { // data conversion
+		// stop the spinner
+        spinner.stop();
 
 		data = {};
 
